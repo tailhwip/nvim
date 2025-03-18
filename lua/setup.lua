eb35a0cc3b7f545e270dcf098768ad10fa99848e
tailhwip <yannick@hey.com>
2025-03-18 11:11:48 +0900

    Add workspace setup commands

---
+++ lua/setup.lua
@@ -0,0 +1,18 @@
+vim.api.nvim_create_user_command("SetupCMakeTools", function()
+  vim.cmd "MasonInstall gersemi"
+  vim.cmd "TSInstall c cpp cmake"
+end, {})
+
+vim.api.nvim_create_user_command("SetupShaderTools", function()
+  vim.cmd "MasonInstall glslls"
+  vim.cmd "TSInstall glsl hlsl"
+end, {})
+
+vim.api.nvim_create_user_command("SetupLuaTools", function()
+  vim.cmd "MasonInstall lua-language-server stylua"
+end, {})
+
+vim.api.nvim_create_user_command("SetupWebTools", function()
+  vim.cmd "MasonInstall html-lsp css-lsp typescript-language-server biome"
+  vim.cmd "TSInstall html css javascript typescript"
+end, {})

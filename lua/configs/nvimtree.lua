34d017d33b5ca6c05ac66574ea87701f31df4017
tailhwip <yannick@hey.com>
2025-08-09 18:07:14 +0900

    Add vim-slueth and hide dotfiles

---
+++ lua/configs/nvimtree.lua
@@ -0,0 +1,5 @@
+return {
+  filters = {
+    dotfiles = true
+  }
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,29 @@
 return {
   {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    opts = require "configs.conform",
-  },
-
-  {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = require "configs.nvimtree"
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "tpope/vim-sleuth",
+    lazy = false
+  }
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 }
-

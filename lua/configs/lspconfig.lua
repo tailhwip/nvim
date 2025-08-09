322bf4d10f44f2ee8d54319a7a641acffeb6e4ca
tailhwip <yannick@hey.com>
2025-08-09 18:04:08 +0900

    Restore lsp float preview styling

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,14 +1,30 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = {
-  -- cmake
+-- Default border style
+vim.cmd "highlight! link FloatBorder CmpBorder"
+vim.cmd "highlight! link NormalFloat CmpDoc"
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = "single"
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+-- Configure LSP servers
+vim.lsp.enable({
+  -- c/cpp
   "clangd",
 
   -- web
   "html",
   "cssls",
   "ts_ls",
+})
+
+vim.lsp.config["clangd"].cmd = {
+  "clangd",
+  "--background-index",
+  "--header-insertion=never",
+  "--clang-tidy",
 }
-
-vim.lsp.enable(servers)
-

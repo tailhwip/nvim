1b11bcd277ae41efe5d0a4bd716e28405788c313
tailhwip <yannick@hey.com>
2025-08-09 18:03:11 +0900

    Add uncrustify, json, and yaml formatters

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,9 +1,10 @@
-return {
+require("conform").setup {
   formatters_by_ft = {
-    -- cmake
-    c = { "clang-format" },
-    cpp = { "clang-format" },
+    -- c/cpp
+    c = { "clang-format", "uncrustify" },
+    cpp = { "clang-format", "uncrustify" },
     cmake = { "gersemi" },
+    slang = { "clang-format", "uncrustify" },
 
     -- web
     html = { "prettier" },
@@ -12,10 +13,26 @@ return {
     javascriptreact = { "prettier" },
     typescript = { "prettier" },
     typescriptreact = { "prettier" },
+
+    -- misc
+    json = { "jq" },
+    yaml = { "yq" },
   },
 
   format_on_save = {
-    timeout_ms = 500,
+    timeout_ms = 2000,
     lsp_fallback = true,
+  },
+
+  formatters = {
+    uncrustify = {
+      command = "uncrustify",
+      args = function(_, ctx)
+        local ft = vim.bo[ctx.buf].filetype:upper()
+        ft = ft == "Slang" and "C" or ft
+        return { "-c", ".uncrustify.cfg", "-q", "-l", ft }
+      end,
+      stdin = true,
+    },
   },
 }
